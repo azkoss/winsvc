4fc1389557a858af115ddb1cc4138371a4a1a061
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-07-07 11:28:59 +0100

    First test of opening ServiceControlManager

--- winsvc.tests/ServiceControlManagerTests.cs
+++ winsvc.tests/ServiceControlManagerTests.cs
@@ -1,6 +1,16 @@
-﻿namespace winsvc.tests
+﻿using NUnit.Framework;
+
+namespace winsvc.tests
 {
+    [TestFixture]
     public class ServiceControlManagerTests
     {
+        [Test]
+        public void OpenControlServiceManager()
+        {
+            using (new ServiceControlManager(null, 0))
+            {
+            }
+        }
     }
 }
