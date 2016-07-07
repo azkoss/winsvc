c0e30234d435f9db9cfd6f16d38fa6eab06d8d35
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-07-07 11:12:19 +0100

    Initial ServiceControlManager class

--- winsvc/NativeMethods.cs
+++ winsvc/NativeMethods.cs
@@ -3,12 +3,19 @@ using System.Runtime.InteropServices;
 
 namespace winsvc
 {
+    // Conventions
+
+    // All declarations are Unicode
+    // BOOL are marshalled as bool
+    // HANDLEs are marshalled as UIntPtr
+    // DWORDs are marshalled as Int32
+
     internal static class NativeMethods
     {
         [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         internal static extern IntPtr OpenSCManager(string machineName, string databaseName, UInt32 desiredAccess);
 
         [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
-        internal static extern bool CloseServiceHandle(IntPtr SCObject);
+        internal static extern bool CloseServiceHandle(IntPtr serviceControlObject);
     }
 }
---
+++ winsvc/ServiceControlManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Win32.SafeHandles;
+
+namespace winsvc
+{
+    public sealed class ServiceControlManager : SafeHandleZeroOrMinusOneIsInvalid
+    {
+        public ServiceControlManager(string machineName, UInt32 desiredAccess) : base(true)
+        {
+            handle = NativeMethods.OpenSCManager(machineName, null, desiredAccess);
+            if (handle == IntPtr.Zero)
+            {
+                throw new Win32Exception();
+            }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            return NativeMethods.CloseServiceHandle(handle);
+        }
+    }
+}
