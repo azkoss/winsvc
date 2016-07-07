6da39908d9906295357df7d9987c19ad5736a309
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-07-07 10:40:55 +0100

    Initial commit

---
+++ winsvc/NativeMethods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace winsvc
+{
+    internal static class NativeMethods
+    {
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern IntPtr OpenSCManager(string machineName, string databaseName, UInt32 desiredAccess);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern bool CloseServiceHandle(IntPtr SCObject);
+    }
+}
