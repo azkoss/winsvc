22461c45d3d6e63b024b02e32e4e4cb61651fc1d
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-07-19 11:39:43 +0100

    Added test to Service.OpenService to make sure we get an exception on trying to open a service that doesn't exist

--- winsvc.tests/ServiceControlManagerTests.cs
+++ winsvc.tests/ServiceControlManagerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
 using dummy_service;
 using NUnit.Framework;
@@ -38,6 +39,23 @@ namespace winsvc.tests
             using (scm.OpenService("Spooler", (UInt32) SERVICE_ACCESS.SERVICE_QUERY_STATUS))
             {
                 // Service is cleaned up in TearDown
+            }
+        }
+
+        [Test]
+        public void OpenServiceFailure()
+        {
+            // Check we get an exception if we try and open a service that doesn't exist
+
+            // ReSharper disable once InconsistentNaming
+            const int ERROR_SERVICE_DOES_NOT_EXIST = 1060;
+
+            using (var scm = ServiceControlManager.OpenServiceControlManager(null, (UInt32) SCM_ACCESS.SC_MANAGER_CONNECT))
+            {
+                // ReSharper disable once AccessToDisposedClosure
+                Assert.That(() => scm.OpenService("Non existant service name", (UInt32) SERVICE_ACCESS.SERVICE_QUERY_STATUS), 
+                    Throws.TypeOf<Win32Exception>()
+                          .With.Property("NativeErrorCode").EqualTo(ERROR_SERVICE_DOES_NOT_EXIST));
             }
         }
 
