ba32aa39d591b8e66950011bd06f8332a2dedc7c
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-12-01 11:40:35 +0000

    Initial work on documentation of IServiceControlManager

--- winsvc/IServiceControlManager.cs
+++ winsvc/IServiceControlManager.cs
@@ -4,11 +4,34 @@ using winsvc.Enumerations;
 using winsvc.Flags;
 using winsvc.Structs;
 
+#pragma warning disable 1584,1711,1572,1581,1580
+
 namespace winsvc
 {
+    /// <summary>  
+    ///  Interface for service control manager.  
+    /// </summary> 
     public interface IServiceControlManager : IDisposable
     {
+        /// <summary>  
+        /// Opens an existing service.  
+        /// </summary> 
+        /// <param name="serviceName">
+        /// The name of the service to be opened. This is the name specified by the lpServiceName parameter of the CreateService
+        /// function when the service object was created, not the service display name that is shown by user interface applications to identify the service.
+        /// 
+        /// The maximum string length is 256 characters.The service control manager database preserves the case of the characters, but service name comparisons
+        /// are always case insensitive. Forward-slash(/) and backslash(\) are invalid service name characters.
+        /// </param>
+        /// <param name="desiredAccess">
+        /// The access to the service.
+        /// Before granting the requested access, the system checks the access token of the calling process against the discretionary access-control
+        /// list of the security descriptor associated with the service object.
+        /// </param>
+        /// <returns>An implementation of IService.</returns>
+        /// <exception cref="Win32Exception">Thrown if the underlying API call fails.</exception>
         IService OpenService(string serviceName, SERVICE_ACCESS desiredAccess);
+
         IService CreateService(string serviceName,
                                 string displayName,
                                 SERVICE_ACCESS desiredAccess,
