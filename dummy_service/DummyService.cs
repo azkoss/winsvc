2d38f9b2a21be63c6daa82b5f6d3b4c8d416de9d
Tony Edgecombe <tonyedgecombe@gmail.com>
2016-12-05 11:14:16 +0000

    Removed redundant code from DummyService

--- dummy_service/DummyService.cs
+++ dummy_service/DummyService.cs
@@ -1,15 +1,11 @@
 ﻿using System.ServiceProcess;
-using System.Threading;
 
 namespace dummy_service
 {
     public class DummyService : ServiceBase
     {
-        private readonly AutoResetEvent _event = new AutoResetEvent(false);
-
         public static string DisplayName = "Dummy Service";
         public static string SvcName = "DummyService";
-        private Thread _thread;
 
         public DummyService()
         {
@@ -17,22 +13,6 @@ namespace dummy_service
             CanShutdown = true;
             CanPauseAndContinue = true;
             CanStop = true;
-        }
-
-        protected override void OnStart(string[] args)
-        {
-            _thread = new Thread(() =>
-            {
-                _event.WaitOne();
-
-            });
-            _thread.Start();
-        }
-
-        protected override void OnStop()
-        {
-            _event.Set();
-            _thread.Join();
         }
     }
 }
